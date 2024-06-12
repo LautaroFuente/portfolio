11929a161d33782f7a58eafd9436d9fb52deae1e
LautaroFuente <lautaro.fuente@yahoo.com>
2024-06-12 18:26:40 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AllProject from "./components/AllProjects";
 
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/proyectos" element={<AllProject />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
